609637f268005a8748093606290017313c96a8a1
Julien Guyomard <guyomard@novius.fr>
2016-04-22 09:39:54 +0200

    apply phpcs-fixer fix

--- src/BadCode.php
+++ src/BadCode.php
@@ -1,47 +1,44 @@
 <?php
 namespace Foo\Bar;
+
 /**
  * A class with bad Code Style
  */
-class BadCode {
+class BadCode
+{
 
 
-	/**
-	 * @param int $var
-	 */
-	protected $var;
+    /**
+     * @param int $var
+     */
+    protected $var;
 
 
-      public function badIndentation()  {
-       $var_with_underscore = "foo";    
+    public function badIndentation()
+    {
+        $var_with_underscore = "foo";
 
-	return null;
-      }
-
-
-	static function no_visibility (){
-		$camelCase = [
-			'foo' 		=> 'bar',
-			'foo-aligned'	=> 'bar'
-		];
-
-		if ( empty($camelCase) )
-		{
-			$ret = TRUE;
-		}
-		else if ( is_array($camelCase)) {
-			$ret = FALSE;
-		} 
-		else 
-		{
-			// nothing
-		}
-
-		return TRUE;
-	}
+        return null;
+    }
 
 
+    public static function no_visibility()
+    {
+        $camelCase = [
+            'foo' => 'bar',
+            'foo-aligned' => 'bar'
+        ];
+
+        if (empty($camelCase)) {
+            $ret = true;
+        } elseif (is_array($camelCase)) {
+            $ret = false;
+        } else {
+            // nothing
+        }
+
+        return true;
+    }
 }
 
-echo "phpcs, how are you?";         
-?>
+echo "phpcs, how are you?";
